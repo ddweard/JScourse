67cb492777d3f44ce81f8b8631f9b2e49514a2ac
ddweard <timurnar2005@gmail.com>
2025-12-24 11:44:05 +0300

    34

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-let name = "John";
-let greeting = `Hello, ${name}!`;
-console.log(greeting); // Hello, John!
+// Function Declaration
+function sum(a, b) { return a + b; }
+
+// Function Expression
+const multiply = function(a, b) { return a * b; };
+
+console.log(multiply(3,2))
+console.log(sum(3,2))   
