ec7e33a0bf03223c3e408d32ae0f46cd592170af
ddweard <timurnar2005@gmail.com>
2025-12-24 11:29:29 +0300

    6

---
+++ index.js
@@ -0,0 +1,5 @@
+const userName = 'Тимур'; // const — значение не изменится
+let userAge = 20;         // let — можно изменить позже
+userAge = 21;              // изменяем значение
+
+console.log(userName, userAge);
